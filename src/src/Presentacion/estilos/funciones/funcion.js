000429b0290ff13d6d44589e548745f4b1562236
Leidy Puerto <ANDRES@10.200.200.227>
2015-10-15 07:22:43 -0500

    Ventanas faltantes y plugin para activar ventanas emergentes

---
+++ src/src/Presentacion/estilos/funciones/funcion.js
@@ -0,0 +1,29 @@
+function ventana(){
+
+    $(".inline").colorbox({inline:true, width:"23%", closeButton:false});
+    $(".callbacks").colorbox({
+        onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+        onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+        onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+        onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+        onClosed:function(){ alert('onClosed: colorbox has completely closed'); },
+
+    });
+    $(this).next('#login-content2').slideToggle();
+    $(this).ToggleClass('active');
+};
+
+
+function ventana2(){
+
+    $(".inline").colorbox({inline:true, width:"26%", closeButton:false});
+    $(".callbacks").colorbox({
+        onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+        onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+        onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+        onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+        onClosed:function(){ alert('onClosed: colorbox has completely closed'); },
+    });
+    $(this).next('#login-content4').slideToggle();
+    $(this).ToggleClass('active');
+};
